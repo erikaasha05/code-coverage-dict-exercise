501ca3b1b42d91767d08df14f13d0671ec5e1936
Irene <choireneh@gmail.com>
2022-09-30 16:58:07 -0400

    added second test for get_student_with_more_classes

--- tests/test_student.py
+++ tests/test_student.py
@@ -44,6 +44,11 @@ def test_get_student_with_more_classes():
         ["mathematics", "foundations of computing"]
     )
 
+    result = get_student_with_more_classes(charles, ada)
+
+    assert result == ada
+
+
     # TODO: write assertions
 
 
